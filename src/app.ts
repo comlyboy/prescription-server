aaaffe7e24b36d6767cb61a46c4454e0535aed54
comlyboy <46368277+comlyboy@users.noreply.github.com>
2020-03-18 07:09:49 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -72,7 +72,7 @@ app.get('/', (req: express.Request, res: express.Response): express.Response => 
             </div>
         </div>
         <nav class="navbar fixed-bottom d-flex justify-content-center bg-dark py-5">
-            <span class="lead mb-0 text-light">Copyright  &#9400;2020. Hospital Plus Inc</span>
+            <span class="lead mb-0 text-light">Copyright  &#9400;2020. Hospiman Inc</span>
         </nav>
     </body>
     <script>
